952431119f7d53bb868b753654ca26ab46bded9d
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-12-16 20:54:15 -0500

    Added a sout message to Daniel.main

--- src/test/java/Com/CybertekGroup28/Tests/Daniel.java
+++ src/test/java/Com/CybertekGroup28/Tests/Daniel.java
@@ -3,6 +3,8 @@ package Com.CybertekGroup28.Tests;
 public class Daniel {
     public static void main(String[] args) {
         //Comment for commit
+
+        System.out.println("Hello World");
     }
 
 
