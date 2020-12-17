0c420bdb64c85e177a92455d6fe0e0cd4fa42f33
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-12-16 21:13:24 -0500

    Came into work today and finished this feature. Ready to go out to MASTER

--- src/test/java/Com/CybertekGroup28/Tests/Daniel.java
+++ src/test/java/Com/CybertekGroup28/Tests/Daniel.java
@@ -7,7 +7,7 @@ public class Daniel {
         System.out.println("Hello World");
         //12/14/2020 Created Date
 
-        System.out.println("second day of wark");
+        System.out.println("second day of work");
         //created12/15/20
         System.out.println("developer working here! be careful!");
         //12/14/20
@@ -15,6 +15,8 @@ public class Daniel {
         System.out.println("SDET working here! dont mess with my code!");
         //12/15/20
 
+        System.out.println("I am daniel and I have come to do work on my feature. Today is 12/16/20");
+        System.out.println("This feature is done. Merging to master and deleting");
 
     }
 
